48bc726fd3d228247556d49857c7c514eba65798
jorgeeldis <jorgeeldisg30@gmail.com>
2026-03-06 21:26:34 -0500

    happyness scripts are done

--- lib/StoicResponses/StoicResponses.cpp
+++ lib/StoicResponses/StoicResponses.cpp
@@ -10,10 +10,13 @@ const String StoicResponses::greetings[] = {
 
 const String StoicResponses::happyness[] = {
     "You seem happy! Today was a good day. I can see you're full of energy and eager to do many things. Channel all those emotions into something great.",
-    ""
+    "Those are great news! There's nothing better than having a good day. Just remember that life is full of ups and downs, so take a moment to enjoy and appreciate these moments.",
+    "I'm happy for you. We both know that life can be hard, and these happy days are what help carry us through. Cherish them, and share that happiness with the people you love."
+
 };
 
 const String StoicResponses::happynessrecom[] = {
-    "You seem happy! Today was a good day. I can see you're full of energy and eager to do many things. Channel all those emotions into something great.",
-    ""
+    "I recommend going out and doing some exercise. Use that great energy to burn some calories and take care of your health. And don't forget to drink some water on your way out!",
+    "I recommend going out and spending some time in nature. Even if you live in a city, try to find a park or spend some time near the ocean. Take a moment to slow down and simply enjoy where you are.",
+    "Let's share this amazing news with someone close to you. Believe me, happiness and kindness are contagious—they'll feel just as happy as you do right now."
 };
