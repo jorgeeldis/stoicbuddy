03a9f7903b4a1e4da8fac0c6278b39b259dd8233
jorgeeldis <jorgeeldisg30@gmail.com>
2026-03-07 13:29:42 -0500

    sadness scripts done

--- lib/StoicResponses/StoicResponses.cpp
+++ lib/StoicResponses/StoicResponses.cpp
@@ -10,13 +10,25 @@ const String StoicResponses::greetings[] = {
 
 const String StoicResponses::happyness[] = {
     "You seem happy! Today was a good day. I can see you're full of energy and eager to do many things. Channel all those emotions into something great.",
-    "Those are great news! There's nothing better than having a good day. Just remember that life is full of ups and downs, so take a moment to enjoy and appreciate these moments.",
-    "I'm happy for you. We both know that life can be hard, and these happy days are what help carry us through. Cherish them, and share that happiness with the people you love."
+    "There's nothing better than having a good day. Just remember that life is full of ups and downs, so take a moment to enjoy and appreciate these moments.",
+    "Amazing! We both know that life can be hard, and these happy days are what help carry us through. Cherish them, and share that happiness with people you love."
 
 };
 
 const String StoicResponses::happynessrecom[] = {
-    "I recommend going out and doing some exercise. Use that great energy to burn some calories and take care of your health. And don't forget to drink some water on your way out!",
-    "I recommend going out and spending some time in nature. Even if you live in a city, try to find a park or spend some time near the ocean. Take a moment to slow down and simply enjoy where you are.",
+    "Let's do some exercise! Use that great energy to burn some calories and take care of your health. And don't forget to drink some water on your way out!",
+    "I recommend going out and spending some time in nature. Even if you live in a city, try to find a park or spend some time near the ocean. Take a moment to chill",
     "Let's share this amazing news with someone close to you. Believe me, happiness and kindness are contagious—they'll feel just as happy as you do right now."
+};
+
+const String StoicResponses::sadness[] = {
+    "That's unfortunate. Life can be rough sometimes, but those challenges can make us stronger when we overcome them. Tomorrow will be a better day—trust me.",
+    "Just as the famous Epictetus said: “Remember that it is not events that disturb people, but their judgments about them.” Take a moment to breathe and reflect.",
+    "Do not suffer before the crisis arrives. Many things trouble us more than they should, before they should, or even when they should not trouble us at all."
+};
+
+const String StoicResponses::sadnessrecom[] = {
+    "I know it may be difficult, but let's try something. Let's work out today—go for a walk or lift some weights. Scientifically, you'll almost certainly feel better afterward.",
+    "Music is the solution to many things, and having a bad day go to a good one is one of them, play your favorite beat up playlist and think about the good memories.",
+    "Text your loved ones—family or close friends—and share how your day went. I'm sure they'll listen and offer support or advice. Go out with them!",
 };
